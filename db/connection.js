ed1c418585848fafe21c7cdb5c22c7fa81d8059a
Taras Khomyn <khomynt97@gmail.com>
2025-02-04 23:56:22 +0100

    fix

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-  mongoose.connect(url);
-};
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+}
 
-module.exports = connectDB;
+module.exports = connectDB
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,26 +1,25 @@
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require('http-status-codes')
 
 const errorHandler = (error, req, res, next) => {
-  console.log("sss", error);
   const errorState = {
-    message: error.message || "Something went wrong",
+    message: error.message || 'Something went wrong',
     statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-  };
+  }
   // password issues
-  if (error.errors?.password?.path === "password") {
-    (errorState.message = error.errors.password.properties.message),
-      (errorState.statusCode = StatusCodes.BAD_REQUEST);
+  if (error.errors?.password?.path === 'password') {
+    ;(errorState.message = error.errors.password.properties.message),
+      (errorState.statusCode = StatusCodes.BAD_REQUEST)
   }
   // last name issues
   if (error.code === 11000) {
-    (errorState.message = "Provided last name already exist"),
-      (errorState.statusCode = StatusCodes.BAD_REQUEST);
+    ;(errorState.message = 'Provided last name already exist'),
+      (errorState.statusCode = StatusCodes.BAD_REQUEST)
   }
-  if (error.kind === "ObjectId") {
-    (errorState.message = "Something is not okay with provided id"),
-      (errorState.statusCode = StatusCodes.BAD_REQUEST);
+  if (error.kind === 'ObjectId') {
+    ;(errorState.message = 'Something is not okay with provided id'),
+      (errorState.statusCode = StatusCodes.BAD_REQUEST)
   }
-  res.status(errorState.statusCode).json({ error: errorState.message });
-};
+  res.status(errorState.statusCode).json({ error: errorState.message })
+}
 
-module.exports = errorHandler;
+module.exports = errorHandler
