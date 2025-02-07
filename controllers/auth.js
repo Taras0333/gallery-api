99b2043f345efc98cd9b8cc7bca1987a1d55aff0
Taras Khomyn <khomynt97@gmail.com>
2025-02-07 22:34:48 +0100

    removed fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,6 +7,8 @@ const createTokenUser = require('../utils/createTokenUser')
 
 const { BadRequestError, UnAuthorizedError, CustomError } = require('../errors')
 
+const removedFields = '-password -verified -isVerified -verificationToken'
+
 const register = async (req, res) => {
   const { body } = req
   const { firstName, lastName, nickName, company, email, password } = body
@@ -43,7 +45,7 @@ const register = async (req, res) => {
     verified: Date.now(),
   })
 
-  user = await User.findById(user.id).select('-password')
+  user = await User.findById(user.id).select(removedFields)
 
   res.status(StatusCodes.CREATED).json({
     user,
@@ -56,7 +58,7 @@ const login = async (req, res) => {
   if (!email || !password) {
     throw new BadRequestError(`Please provide email and password`)
   }
-  let user = await User.findOne({ email })
+  let user = await User.findOne({ email }).select(removedFields)
 
   if (!user) {
     throw new BadRequestError(`There is no user with provided email: ${email}`)
--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 
+const removedFields = '-password -verified -isVerified -verificationToken'
+
 const getAllUsers = async (_, res) => {
   const users = await User.find({}).select('-password')
 
@@ -13,7 +15,7 @@ const getUserById = async (req, res) => {
   const {
     params: { id },
   } = req
-  const user = await User.findById(id).select('-password')
+  const user = await User.findById(id).select(removedFields)
 
   res.status(StatusCodes.OK).json({ user })
 }
