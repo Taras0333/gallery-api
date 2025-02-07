e2b547d16c072b968d1751cc11acdb41c5711aee
Taras Khomyn <khomynt97@gmail.com>
2025-02-07 15:34:21 +0100

    fixed responce codes

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,10 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = async (_, res) => {
   const users = await User.find({}).select('-password -__v')
 
-  res.status(StatusCodes.CREATED).json({
+  res.status(StatusCodes.OK).json({
     users,
     count: users.length,
   })
@@ -15,7 +15,7 @@ const getUserById = async (req, res) => {
   } = req
   const user = await User.findById(id).select('-password -__v')
 
-  res.status(StatusCodes.CREATED).json({ user })
+  res.status(StatusCodes.OK).json({ user })
 }
 
 module.exports = { getAllUsers, getUserById }
