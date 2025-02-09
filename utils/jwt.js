ea538731c3e375c74bea48029d856623f8d7ed0c
Taras Khomyn <khomynt97@gmail.com>
2025-02-09 01:58:06 +0100

    console

--- utils/jwt.js
+++ utils/jwt.js
@@ -8,6 +8,7 @@ const createJWT = ({ payload }) => {
 }
 
 const attachCookiesToResponse = ({ res, domain, user, refreshToken }) => {
+  console.log('domain', domain)
   const isLocalCall = domain.includes('localhost')
   const accessTokenJWT = createJWT({ payload: { user } })
   const refreshTokenJWT = createJWT({ payload: { user, refreshToken } })
@@ -17,19 +18,17 @@ const attachCookiesToResponse = ({ res, domain, user, refreshToken }) => {
 
   res.cookie('accessToken', accessTokenJWT, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production' && isLocalCall,
+    secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite:
-      process.env.NODE_ENV === 'production' && isLocalCall ? 'none' : 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     expires: new Date(Date.now() + oneDay),
   })
 
   res.cookie('refreshToken', refreshTokenJWT, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production' && isLocalCall,
+    secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite:
-      process.env.NODE_ENV === 'production' && isLocalCall ? 'none' : 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     expires: new Date(Date.now() + longerExp),
   })
 }
